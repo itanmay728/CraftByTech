d9980ba38848acae2fab6a1a9677b246e7b6f508
itanmay728 <ktanmay1130@gmail.com>
2025-11-11 15:25:47 +0530

    bug fix

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,26 +1,42 @@
-import React from 'react'
-import Hero from '../../components/Hero/Hero'
-import HeroScroll from '../../components/Hero/HeroScroll';
-import WhyUs from '../../components/WhyUs/WhyUs';
-import LightningFast from '../../components/LightningFast/LightningFast';
-import Testimonials from '../../components/Testimonials/Testimonials';
-import ServiceSection from '../../components/ServiceSection/ServiceSection';
-import ClientsMarquee from '../../components/ClientsMarquee/ClientsMarquee';
-import ContactForm from '../../components/ContactForm/ContactForm';
+import React from "react";
+import Hero from "../../components/Hero/Hero";
+import HeroScroll from "../../components/Hero/HeroScroll";
+import WhyUs from "../../components/WhyUs/WhyUs";
+import LightningFast from "../../components/LightningFast/LightningFast";
+import Testimonials from "../../components/Testimonials/Testimonials";
+import ServiceSection from "../../components/ServiceSection/ServiceSection";
+import ClientsMarquee from "../../components/ClientsMarquee/ClientsMarquee";
+import ContactForm from "../../components/ContactForm/ContactForm";
+import styles from "./Home.module.css";
 
 const Home = () => {
   return (
-    <div>
-        <Hero/>
-        <HeroScroll/>
-        <WhyUs/>
-        <LightningFast/>
-        <ServiceSection/>
-        <Testimonials/>
-        <ContactForm/>
-        <ClientsMarquee/>
-    </div>
-  )
-}
+    <main className={styles.home}>
+      {/* ==== HERO SECTION ==== */}
+      <Hero />
+
+      {/* ==== SCROLL INDICATOR / SUB HERO ==== */}
+      <HeroScroll />
+
+      {/* ==== WHY CHOOSE US ==== */}
+      <WhyUs />
+
+      {/* ==== PERFORMANCE SECTION ==== */}
+      <LightningFast />
+
+      {/* ==== SERVICES ==== */}
+      <ServiceSection />
+
+      {/* ==== TESTIMONIALS ==== */}
+      <Testimonials />
+
+      {/* ==== CONTACT FORM ==== */}
+      <ContactForm />
+
+      {/* ==== CLIENTS ==== */}
+      <ClientsMarquee />
+    </main>
+  );
+};
 
 export default Home;
